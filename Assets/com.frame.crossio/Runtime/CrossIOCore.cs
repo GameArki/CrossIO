84a1a5a2fd499979668466c9b7219fa1d2fa6b58
杰克有茶 <chenwansal1@163.com>
2022-12-08 16:47:40 +0800

    <feature> add: Read & Write To Normal Direction

--- Assets/com.frame.crossio/Runtime/CrossIOCore.cs
+++ Assets/com.frame.crossio/Runtime/CrossIOCore.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using System.Threading.Tasks;
 using UnityEngine;
 
@@ -5,6 +6,18 @@ namespace JackFrame.CrossIO {
 
     public static class CrossIOCore {
 
+        // ==== Normal ====
+        public static void WriteToNormalDir<T>(CrossIODataType dataType, string dir, string filename, T obj) {
+            var bytes = ToBytes(dataType, obj);
+            File.WriteAllBytes(Path.Combine(dir, filename), bytes);
+        }
+
+        public static T ReadFromNormalDir<T>(CrossIODataType dataType, string dir, string filename) {
+            var bytes = File.ReadAllBytes(Path.Combine(dir, filename));
+            return FromBytes<T>(dataType, bytes);
+        }
+
+        // ==== Persistent ====
         public static void WriteToPersistent<T>(CrossIODataType dataType, string dir, string filename, T obj) {
             var bytes = ToBytes(dataType, obj);
             UnityFileHelper.WriteFileBytesToPersistentDataPath(dir, filename, bytes);
@@ -15,6 +28,7 @@ namespace JackFrame.CrossIO {
             return FromBytes<T>(dataType, bytes);
         }
 
+        // ==== StreamingAssets ====
         public async static Task<T> ReadFromStreamingAssets<T>(CrossIODataType dataType, string dir, string filename) {
             var bytes = await UnityFileHelper.ReadFileBytesFromStreamingAssetsAsync(dir, filename);
             return FromBytes<T>(dataType, bytes);
